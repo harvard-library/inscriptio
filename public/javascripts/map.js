599f267d4814ee67908d9767334dab8610dc52c7
Justin Clark <jclark@cyber.law.harvard.edu>
2011-06-13 17:39:18 -0400

    Refactored and added more tooltip functionality

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,62 +1,71 @@
 floorMap = {
-	newestOverlay: 0,
 	overlays: [],
 	overlay: function (type, x1, y1, x2, y2, width, height) {
-		this.type	= type || 'asset';
-		this.id		= 'overlay' + floorMap.overlays.length;
-		this.x1		= x1 || 0;
-		this.y1		= y1 || 0;
-		this.x2		= x2 || this.x1;
-		this.y2		= y2 || this.y1;
-		this.assignedTo	= null;
+		var type	= type || 'asset',
+		id		= 'overlay' + floorMap.overlays.length,
+		x1		= x1 || 0,
+		y1		= y1 || 0,
+		x2		= x2 || this.x1,
+		y2		= y2 || this.y1,
+		assignedTo	= null;
 
-		this.getHtml = function() {
-			return '<div id="' + this.id + '" class="overlay ' + this.type 
-				+ '" style="z-index: ' + floorMap.overlays.length + ';' 
-				+ ' left: ' + this.x1 + 'px;'
-				+ ' top: ' + this.y1 + 'px;'
-				+ ' right: ' + ($(window).width() - this.x2) + 'px;'
-				+ ' bottom: ' + ($(window).height() - this.y2) + 'px;'
-				+ '"></div>';
-		}
+		var newOverlay = $('<div id="overlay' + floorMap.overlays.length + '" class="overlay ' + type + '"' 
+			+ ' style="z-index: ' + floorMap.overlays.length + ';' 
+			+ ' left: ' + x1 + 'px;'
+			+ ' top: ' + y1 + 'px;'
+			+ ' right: ' + ($(window).width() - x2) + 'px;'
+			+ ' bottom: ' + ($(window).height() - y2) + 'px;'
+			+ '"></div>').data({
+			'type': type,
+			'assignedTo': assignedTo
+			});
 
-		floorMap.overlays.push(this);
-		floorMap.newestOverlay = this.id;
-	},
+		floorMap.overlays.push(newOverlay);
+		return newOverlay;
+	}
 };
 $(function() {
+	$('#closeTooltip').click(function() {
+		$('#tooltip').hide();
+	});
+
 	$('#removeOverlay').click(function() {
 		$('#' + $('#tooltip').data('currentOverlay')).remove();
 		$('#tooltip').hide();
 	});
 
+	$('#applyOverlay').click(function() {
+		/* Ajax stuff */
+		$('#' + $('#tooltip').data('currentOverlay')).data({
+			type: $('#overlayType').val(),
+			assignedId: $('#overlayId').val()
+		});
+	});
+
 	$('#map').mousedown(function(event) {
 		if (event.target == this) {
-			var newOverlay = new floorMap.overlay(
-				'asset', event.pageX, event.pageY
-			);
-			$(this).append(newOverlay.getHtml()).mousemove(function(event) { 
-				var thisOverlay = $('#' + floorMap.newestOverlay);
-				if (event.pageY > parseInt($('#' + floorMap.newestOverlay).css('top')))
-					$('#' + floorMap.newestOverlay).css('bottom', $(window).height() - event.pageY);
-				else
-					$('#' + floorMap.newestOverlay).css('top', event.pageY);
+			var newOverlay = new floorMap.overlay( $('#overlayType').val(), event.pageX, event.pageY );
 
-				if (event.pageX > parseInt(thisOverlay.css('left')))
-					$('#' + floorMap.newestOverlay).css('right', $(window).width() - event.pageX);
+			$(this).append($(newOverlay)).mousemove(function(event) { 
+				if (event.pageY > parseInt($(newOverlay).css('top')))
+					$(newOverlay).css('bottom', $(window).height() - event.pageY);
 				else
-					$('#' + floorMap.newestOverlay).css('left', event.pageX);
+					$(newOverlay).css('top', event.pageY);
+				if (event.pageX > parseInt($(newOverlay).css('left')))
+					$(newOverlay).css('right', $(window).width() - event.pageX);
+				else
+					$(newOverlay).css('left', event.pageX);
 			}).one('mouseup', function() {
-				$('#' + floorMap.newestOverlay).css({
-					width: parseInt($('#' + floorMap.newestOverlay).innerWidth()) + 'px',
-					height: parseInt($('#' + floorMap.newestOverlay).innerHeight()) + 'px',
+				$(newOverlay).css({
+					width: parseInt($(newOverlay).innerWidth()) + 'px',
+					height: parseInt($(newOverlay).innerHeight()) + 'px',
 					right: 'auto',
 					bottom: 'auto'
 				});
 			});
-			$('#tooltip').data({ 'currentoverlay': floorMap.newestOverlay });
+			$('#tooltip').data({ 'currentoverlay': $(newOverlay).attr('id') });
 
-			$('#' + floorMap.newestOverlay).drag(function(event) {
+			$(newOverlay).drag(function(event) {
 				$(this).css({
 					top: event.pageY - $(this).data('diffY'),
 					left: event.pageX - $(this).data('diffX')
@@ -68,7 +77,8 @@ $(function() {
 				});
 			}).click(function() {
 				$('#tooltip').data({ 'currentOverlay': $(this).attr('id') });
-				$('#overlayType option[value="' + $(this).attr('id')).val();
+				$('#overlayType').val($(this).data('type'));
+				$('#overlayId').val($(this).data('assignedId'));
 				$('#tooltip').css({
 					left: $(this).offset().left + 10 + $(this).outerWidth(),
 					top: $(this).offset().top
@@ -81,25 +91,29 @@ $(function() {
 	});
 
 
-	$('#closeTooltip').click(function() {
-		$('#tooltip').hide();
-	});
 	$('#overlayType').change(function() {
-		if ($(this).val() == 'asset') {
-			$('#' + $('#tooltip').data('currentOverlay')).removeClass('subject').addClass('asset');
-			$('#overlayIdLabel').html('Asset: ');
-			$('#overlayId').empty();
-			for (i in floorMap.assets) {
-				$('#overlayId').append('<option value="' + floorMap.assets[i].id + '">' + floorMap.assets[i].name + '</option>');
-			}
+		var tipBits = {
+			'typeVal': 'asset', 
+			'typeName': 'Assets',
+			'typeCollection': floorMap.assets,
+			'oldTypeVal': 'subject'
+		};
+		if ($(this).val() == 'subject') {
+			tipBits = {
+				'typeVal': 'subject', 
+				'typeName': 'Subjects',
+				'typeCollection': floorMap.subjects,
+				'oldTypeVal': 'asset'
+			};
 		}
-		else {
-			$('#' + $('#tooltip').data('currentOverlay')).removeClass('asset').addClass('subject');
-			$('#overlayIdLabel').html('Subject: ');
-			$('#overlayId').empty();
-			for (i in floorMap.subjects) {
-				$('#overlayId').append('<option value="' + floorMap.subjects[i].id + '">' + floorMap.subjects[i].name + '</option>');
-			}
+		$('#' + $('#tooltip').data('currentOverlay')).removeClass(tipBits.oldTypeVal).addClass(tipBits.typeVal);
+		$('#overlayIdLabel').html(tipBits.typeName + ': ');
+		$('#overlayId').empty();
+		for (i in tipBits.typeCollection) {
+			$('#overlayId').append(
+				'<option value="' + tipBits.typeCollection[i].id + '">' 
+				+ tipBits.typeCollection[i].name 
+				+ '</option>');
 		}
 	});
 });
