a3169b6d15899e3ac4baba33b097a34f9502c704
Anita <anita@anitapatel.com>
2011-07-15 16:27:17 -0400

    added sendmail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Inscriptio::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.delivery_method = :sendmail
 end
