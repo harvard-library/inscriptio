5f48ace732e62919cfe230d1079bff5cd615c144
Anita <anita@anitapatel.com>
2011-08-08 16:23:46 -0400

    fixed issue with creating messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   
   def display_message(message_key = :default)
     begin
-      Message.find_by_description(message_key.to_s)
+      Message.find_by_description!(message_key.to_s)
     rescue
-      Message.find_by_description(:default)
+      Message.find_by_description!(:default)
     end
   end
 end
