0f7237df46f661b71f70094959f3069caa0c5324
Anita <anita@anitapatel.com>
2011-11-21 13:24:45 -0500

    fixing root URL for dev

--- config/initializers/00_inscriptio_init.rb
+++ config/initializers/00_inscriptio_init.rb
@@ -1,4 +1,5 @@
-ROOT_URL = 'inscriptio.harvard.edu'
+#ROOT_URL = 'inscriptio.harvard.edu'
+ROOT_URL = 'dev11.berkmancenter.org'
 ROOT_PATH = '/'
 DEFAULT_MAILER_SENDER = "wcarrel@fas.harvard.edu"
 #how many email records to pull at a time
