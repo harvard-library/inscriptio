9300ef97ea69099c81e9596946aa68ac803d12a2
Dave Mayo <dave_mayo@harvard.edu>
2013-11-27 15:13:38 -0500

    Fix bug in mail forms, also tell dev to stop sending emails.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,9 @@ Inscriptio::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  # Don't actually send emails
+  config.action_mailer.delivery_method = :test
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
