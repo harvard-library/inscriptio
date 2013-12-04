a0182df9690e836bdbb7d198f538006ee7a82030
Dave Mayo <dave_mayo@harvard.edu>
2013-12-04 09:22:19 -0500

    LTSINSCRIP-48 #resolve also fixes similar problem with bulletin boards which likely never manifests.

--- app/controllers/bulletin_boards_controller.rb
+++ app/controllers/bulletin_boards_controller.rb
@@ -1,9 +1,9 @@
 class BulletinBoardsController < ApplicationController
   before_filter :authenticate_admin!, :except => [:index, :show]
-  
+
   def index
     @bulletin_boards = BulletinBoard.all
-    
+
     bbs = BulletinBoard.find(:all, :conditions => {:post_lifetime => 1})
     bbs.each do |bb|
       bb.post_lifetime = 30
@@ -18,21 +18,21 @@ class BulletinBoardsController < ApplicationController
   def show
     @bulletin_board = BulletinBoard.find(params[:id])
     @posts = Post.find(:all, :conditions => {:bulletin_board_id => @bulletin_board.id})
-    
+
     breadcrumbs.add 'Bulletin Board', @bulletin_board.id
   end
 
   def edit
     @bulletin_board = BulletinBoard.find(params[:id])
   end
-  
+
   def create
     @bulletin_board = BulletinBoard.new
     @bulletin_board.attributes = params[:bulletin_board]
     respond_to do|format|
       if @bulletin_board.save
         flash[:notice] = 'Added that Bulletin Board'
-        format.html {render :action => :index}
+        format.html {redirect_to :action => :index}
       else
         flash[:error] = 'Could not add that Bulletin Board'
         format.html {render :action => :new}
@@ -55,7 +55,7 @@ class BulletinBoardsController < ApplicationController
     respond_to do|format|
       if @bulletin_board.save
         flash[:notice] = %Q|Bulletin Board #{@bulletin_board.id} updated!|
-        format.html {render :action => :index}
+        format.html {redirect_to :action => :index}
       else
         flash[:error] = 'Could not update that Bulletin Board'
         format.html {render :action => :new}
--- app/controllers/subject_areas_controller.rb
+++ app/controllers/subject_areas_controller.rb
@@ -1,9 +1,9 @@
 class SubjectAreasController < ApplicationController
   before_filter :authenticate_admin!, :except => [:index, :show]
-  
+
   def index
     @subject_areas = SubjectArea.all
-    
+
     breadcrumbs.add "Subject Areas"
   end
 
@@ -11,13 +11,10 @@ class SubjectAreasController < ApplicationController
     @subject_area = SubjectArea.new
     @floors = Array.new
     Floor.all.collect {|f| @floors << ["#{f.name} - #{f.library.name}", f.id] }
-    p "floors"
-    p @floors
   end
 
   def show
     @subject_area = SubjectArea.find(params[:id])
-    
     breadcrumbs.add @subject_area.floors[0].library.name, library_path(@subject_area.floors[0].library.id)
     breadcrumbs.add @subject_area.name, @subject_area.id
   end
@@ -25,7 +22,7 @@ class SubjectAreasController < ApplicationController
   def edit
     @subject_area = SubjectArea.find(params[:id])
   end
-  
+
   def create
     @subject_area = SubjectArea.new
     @subject_area.attributes = params[:subject_area]
@@ -55,7 +52,7 @@ class SubjectAreasController < ApplicationController
     respond_to do|format|
       if @subject_area.save
         flash[:notice] = %Q|#{@subject_area} updated|
-        format.html {render :action => :index}
+        format.html {redirect_to :action => :index}
       else
         flash[:error] = 'Could not update that Subject Area'
         format.html {render :action => :new}
