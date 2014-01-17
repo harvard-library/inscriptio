harvard-library/inscriptio

commit b065e2cf37a8ea11d711669129e9e68cbe0b66bf

LTSINSCRIP-76 , LTSINSCRIP-75 (sort of) and more RailsUJS stuff
This branch consists of a marginally cleaner and more performant rewrite of the
functionality on the "Reports" admin page.  I've located it at Reservation#index, because
there's no user listing of reservations, and it seemed to fit.

Additionally, delete and update functionality for reservations is handled
via ajax (Rails UJS, to be specific).  I'm doing it in the low-pass way, by letting
the Rails UJS helper eval returned JS.  We have CSRF protection enabled, so it's not
insecure, but arguably it could be done more cleanly by hanging off the 'ajax:success'
and 'ajax:failure' methods.  Not gonna, for now.

app/controllers/reservations_controller.rb
@@ -1,6 +1,7 @@
 class ReservationsController < ApplicationController
   before_filter :verify_credentials, :only => [:index, :new, :show, :create, :update, :destroy]
   before_filter :process_dates, :only => [:create, :update]
+  before_filter :authenticate_admin!, :only => [:index]
 
   def process_dates
     params[:reservation][:start_date] = Date.strptime(params[:reservation][:start_date], "%m/%d/%Y")
@@ -8,12 +9,29 @@ class ReservationsController < ApplicationController
   end
 
   def index
-    if current_user.admin?
-      @reservations = Reservation.all
-    else
-      @pending = Reservation.find(:all, :conditions => {:user_id => current_user.id, :status_id => Status.find(:first, :conditions => ["lower(name) = 'pending'"])}, :order => ['created_at DESC'])
-      @active = Reservation.find(:all, :conditions => {:user_id => current_user.id, :status_id => Status.find(:first, :conditions => ["lower(name) = 'approved'"])}, :order => ['created_at DESC'])
-      @expired = Reservation.find(:all, :conditions => {:user_id => current_user.id, :status_id => Status.find(:first, :conditions => ["lower(name) = 'expired'"])}, :order => ['created_at DESC'])
+    @statuses = Status.where(:name => ['Pending', 'Expired', 'Approved']).select([:name, :id]).reduce({}) {|acc, el| acc[el.name] = el.id;acc }
+    rel = Library.includes(:reservable_asset_types => {:reservable_assets => {:reservations => [:status, :user]}})
+    rel = rel.where("statuses.id IN (?)", @statuses.values)
+    rel = rel.where('reservations.deleted_at IS NULL')
+    @libraries = rel.order('libraries.id, reservable_asset_types.id, users.email')
+
+    # The output of the following nested reduce is a nested hash of arrays of Reservations,
+    # keyed by [:library_id][:reservable_asset_type_id][:status_name]
+    @reservations = @libraries.reduce({}) do |libs, l|
+      libs[l.id] = l.reservable_asset_types.reduce({}) do |rats, rat|
+        rats[rat.id] = rat.reservable_assets.reduce({}) do |ras, ra|
+          ra.reservations.each do |r|
+            case r.status_id
+            when @statuses['Pending']
+              ras['Pending'] ? ras['Pending'].push(r) : (ras['Pending'] = [r])
+            when @statuses['Expired']
+              ras['Expired'] ? ras['Expired'].push(r) : (ras['Expired'] = [r])
+           when @statuses['Approved']
+              ras['Approved'] ? ras['Approved'].push(r) : (ras['Approved'] = [r])
+            end
+          end;ras
+        end;rats
+      end;libs
     end
 
     breadcrumbs.add 'Reservations'
@@ -143,6 +161,19 @@ class ReservationsController < ApplicationController
         flash[:error] = 'Could not update that Reservation'
         format.html {redirect_to new_reservation_path(:reservable_asset => @reservation.reservable_asset)}
       end
+    end
+  end
+
+  def approve
+    relation = Reservation
+    relation = relation.with_deleted if current_user.admin?
+    @reservation = relation.find(params[:id])
+
+    @reservation.status = Status.find_by_name('Approved')
+    @reservation.save
+
+    respond_to do |format|
+      format.js
     end
   end
 end

app/models/reservation.rb
@@ -12,6 +12,21 @@ class Reservation < ActiveRecord::Base
   after_save :post_save_hooks, :except => [:archive]
   after_destroy :post_destroy_hooks
 
+  scope :status, ->(s) {
+    rel = joins(:status)
+
+    case s
+    when String
+      rel.where('statuses.name = ?', s)
+    when Symbol
+      rel.where('statuses.name = ?', s.to_s.split('_').map(&:capitalize).join(' '))
+    when Array
+      rel.where('statuses.name IN (?)', s.map {|s| s.to_s.split('_').map(&:capitalize).join(' ')})
+    else
+      raise "I don't understand that as a status or list of statuses"
+    end
+  }
+
   def post_save_hooks
     notice = ReservationNotice.find(:first, :conditions => {:status_id => self.status_id, :reservable_asset_type_id => self.reservable_asset.reservable_asset_type.id, :library_id => self.reservable_asset.reservable_asset_type.library.id})
     Email.create(

app/views/layouts/application.html.erb
@@ -55,16 +55,17 @@ end %>
 
 			 <% if current_user.try(:admin?) %>
 			   <li><%= link_to "Messages", messages_path %></li>
-	  	       <li><%= link_to "Users", users_path %></li>
-	           <li><%= link_to "User Types", user_types_path %></li>
-			   <li><%= link_to "School Affiliations", school_affiliations_path %></li>
+	  	     <li><%= link_to "Users", users_path %></li>
+	         <li><%= link_to "User Types", user_types_path %></li>
+			     <li><%= link_to "School Affiliations", school_affiliations_path %></li>
 
-			   <li><%= link_to "Subject Areas", subject_areas_path %></li>
-			   <li><%= link_to "Call Numbers", call_numbers_path %></li>
+			     <li><%= link_to "Subject Areas", subject_areas_path %></li>
+			     <li><%= link_to "Call Numbers", call_numbers_path %></li>
 
-	           <li><%= link_to "Reports", reports_path %></li>
-	           <li><%= link_to "Reservable Assets", reservable_asset_types_path %></li>
-	           <li><%= link_to "Reservation Notices", reservation_notices_path %></li>
+           <li><%= link_to "Reservations", reservations_path %></li>
+	         <li><%= link_to "Reports", reports_path %></li>
+	         <li><%= link_to "Reservable Assets", reservable_asset_types_path %></li>
+	         <li><%= link_to "Reservation Notices", reservation_notices_path %></li>
 	         <% end %>
 	         <li><%= link_to "Help", help_messages_path %></li>
 			</ul>

app/views/reservations/destroy.js.erb
@@ -1,5 +1,5 @@
 (function () {
-  var sel = '#reservation-' + <%= @reservable_asset.id %>;
+  var sel = '#reservation-' + <%= @reservation.id %>;
   if ($(sel).siblings('li').length == 0) {
     $(sel).replaceWith('<li class="empty">No reservations.</li>');
   }

app/views/reservations/index.html.erb
@@ -1,27 +1,30 @@
 <% content_for :header do %>Reservations<% end %>
 <div class="reservations collection-list">
-  <% unless @pending.blank? %>
-    <h2>Pending Reservations</h2>
-	<%= render :partial => 'shared/lines/reservation', :collection => @pending %>
-  <% end %>
-		
-  <% unless @active.blank? %>
-    <h2>Current Reservations</h2>
-	<%= render :partial => 'shared/lines/reservation', :collection => @active %>
-  <% end %>
-
-  <% unless @expired.blank? %>
-    <h2>Expired Reservations</h2>
-	<%= render :partial => 'shared/lines/reservation', :collection => @expired %>
-  <% end %>
 
   <% if current_user.admin? %>
-    <% unless @reservations.blank? %>
-	  <h2>Reservations</h2>
-	  <%= render :partial => 'shared/lines/reservation', :collection => @reservations %>
-	<% else %>
-	  <h2>No reservations yet.</h2>
-	<% end %>
-    <h2 class="action-button"><%= link_to('New Reservation', new_reservation_path) %></h2>
+  <h2 class="action-button"><%= link_to('New Reservation', new_reservation_path) %></h2>
   <% end %>
+
+
+  <% @libraries.each do |l| %>
+    <div>
+      <h2><%= l.name %></h2>
+      <div class="lib" id="library-<%= l.id %>">
+      <% l.reservable_asset_types.each do |rat| %>
+        <h3><%= rat.name %></h3>
+        <div class="rat" id="rat-<%= rat.id %>">
+        <% res = @reservations[l.id][rat.id] %>
+        <% ['Pending', 'Expired', 'Approved'].each do |status| %>
+          <% unless res[status].blank? %>
+          <h2 class='collapsible'><%= status %> Reservations <%= raw "(<span class='count'>#{res[status].length}</span>)" %></h2>
+          <ul class="<%= status.downcase %>">
+	          <%= render :partial => 'shared/lines/reservation', :collection => res[status] %>
+          </ul> <!-- end <%= status.downcase %> -->
+          <% end %>
+        <% end %>
+      </div> <!-- END rat-<%= rat.id %> -->
+      <% end %>
+      <!-- END library-<%= l.id %> -->
+  <% end %>
+
 </div>

app/views/shared/lines/_reservation.html.erb
@@ -1,11 +1,14 @@
 <li class="reservation line" id="reservation-<%= reservation.id %>">
-  ID: <%= link_to("#{reservation.id}", reservation ) %>,
+  ID: <%= link_to("#{reservation.id}", (reservation.allow_edit?(current_user) || current_user.admin? ? edit_reservation_path(reservation) : reservation) ) %>,
   User: <%= link_to("#{[reservation.user.first_name, reservation.user.last_name].join(' ')} (#{reservation.user.email})", reservation.user) %>,
   <%= "from #{reservation.start_date} to #{reservation.end_date}" %>
   <ul class="actions">
     <% if current_user.admin? || reservation.allow_edit?(current_user) %>
-    <li><%= link_to('Edit', edit_reservation_path(reservation), :class => "action-edit") %></li>
-    <li><%= link_to(reservation.deleted_at ? 'Delete' : 'Clear', reservation_path(reservation), :confirm => "Are you sure you want to #{reservation.deleted_at ? 'delete' : 'Clear'} this reservation?", :method => :delete, :id => "delete-#{reservation.id}", :class => "action-delete") %></li>
+      <% if reservation.status_id == @statuses['Pending'] %>
+        <li><%= link_to('Approve', approve_reservation_path(reservation), :class => "action-approve", :status_id => @statuses['Approved'], :approve => true, :remote => true, :method => :put) %></li>
+      <% end %>
+
+    <li><%= link_to(reservation.deleted_at ? 'Delete' : 'Clear', reservation_path(reservation), :confirm => "Are you sure you want to #{reservation.deleted_at ? 'delete' : 'Clear'} this reservation?", :method => :delete, :id => "delete-#{reservation.id}", :class => "action-delete", :remote => true) %></li>
     <% end %>
   </ul>
 </li>

config/routes.rb
@@ -40,7 +40,11 @@ Inscriptio::Application.routes.draw do
 
   resources :user_types
 
-  resources :reservations
+  resources :reservations do
+    member do
+      put 'approve'
+    end
+  end
 
 
   resources :search

commit d5d7af2351f45f6746f85f8ee4a1719150362f79

Fixes for problems introduced in b065e2cf37a8ea11d711669129e9e68cbe0b66bf, mostly discussed in  CR-LTSINSCRIP-8
The noted exception - the new users/reservations action is needed because previously the index view was used for users and admins.

app/controllers/reservations_controller.rb
@@ -9,7 +9,11 @@ class ReservationsController < ApplicationController
   end
 
   def index
-    @statuses = Status.where(:name => ['Pending', 'Expired', 'Approved']).select([:name, :id]).reduce({}) {|acc, el| acc[el.name] = el.id;acc }
+    @statuses = Status.
+      where(:name => ['Pending', 'Expired', 'Approved']).
+      select([:name, :id]).
+      reduce({}) {|statuses, s| statuses[s.name] = s.id; statuses }
+
     rel = Library.includes(:reservable_asset_types => {:reservable_assets => {:reservations => [:status, :user]}})
     rel = rel.where("statuses.id IN (?)", @statuses.values)
     rel = rel.where('reservations.deleted_at IS NULL')

app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 require 'csv'
 
 class UsersController < ApplicationController
-  before_filter :authenticate_admin!, :except => [:edit, :update]
-  
+  before_filter :authenticate_admin!, :except => [:edit, :update, :reservations]
+
   def index
     @users = User.find(:all, :order => ['created_at ASC'])
     breadcrumbs.add 'Users'
@@ -11,11 +11,11 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    
+
     breadcrumbs.add 'Users', users_path
     breadcrumbs.add 'New'
   end
-  
+
   def create
     @user = User.new
     @user.user_type_id = params[:user][:user_type_id]
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
     @user.first_name = params[:user][:first_name]
     @user.last_name = params[:user][:last_name]
     params[:user][:admin] == "1" ? @user.admin = true : @user.admin = false
-    
+
     respond_to do|format|
       if @user.save
         flash[:notice] = 'Added that User'
@@ -40,18 +40,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reservations = Reservation.find(:all, :conditions => {:user_id => @user.id}, :order => ['created_at DESC'])
-    
+
     breadcrumbs.add 'Users', users_path
     breadcrumbs.add @user.email, @user.id
   end
 
   def edit
     @user = User.find(params[:id])
-    
+
     unless current_user.admin? || @user.email == current_user.mail
        redirect_to('/') and return
     end
-     
+
     breadcrumbs.add 'Users', users_path
     breadcrumbs.add @user.email, @user.id
     breadcrumbs.add 'Edit'
@@ -59,11 +59,11 @@ class UsersController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
-    
+
     unless current_user.admin? || @user.email == current_user.mail
        redirect_to('/') and return
     end
-    
+
     user = @user.email
     if @user.destroy
       flash[:notice] = %Q|Deleted user #{user}|
@@ -75,13 +75,13 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    
+
     unless current_user.admin? || @user.email == current_user.mail
        redirect_to('/') and return
     end
-    
+
     @user.attributes = params[:user]
-    params[:user][:admin] == "1" ? @user.admin = true : @user.admin = false  
+    params[:user][:admin] == "1" ? @user.admin = true : @user.admin = false
     respond_to do|format|
       if @user.save
         flash[:notice] = %Q|#{@user} updated|
@@ -92,11 +92,11 @@ class UsersController < ApplicationController
       end
     end
   end
-  
+
   def import
     @file = params[:upload][:datafile] unless params[:upload].blank?
     CSV.parse(@file.read).each do |cell|
-      @user = User.new  
+      @user = User.new
       user_type = UserType.find(cell[0].to_i)
       school_affiliation = SchoolAffiliation.find(cell[1].to_i)
 
@@ -111,7 +111,7 @@ class UsersController < ApplicationController
     end
     redirect_to users_path
   end
-  
+
   def export
     @users = User.find(:all, :order => ['email ASC'])
     CSV.open("#{RAILS_ROOT}/public/uploads/users.csv", "w") do |csv|
@@ -122,6 +122,16 @@ class UsersController < ApplicationController
     @csv = true
     flash[:notice] = 'Exported!'
     redirect_to users_path(:csv => @csv)
-  end  
+  end
+
+  def reservations
+    @user = User.find(params[:id])
+    @statuses = Status.
+      where(:name => ['Pending', 'Expired', 'Approved']).
+      select([:name, :id]).
+      reduce({}) {|statuses, s| statuses[s.name] = s.id; statuses }
+
+    @reservations = @user.reservations.status([:pending,:approved]).group_by {|r| r.status.name}
+  end
 
 end

app/models/reservation.rb
@@ -12,6 +12,8 @@ class Reservation < ActiveRecord::Base
   after_save :post_save_hooks, :except => [:archive]
   after_destroy :post_destroy_hooks
 
+  # This scope is meant to allow for easily selecting reservations by
+  # status names, via the usual options of keyword, string, and array of same
   scope :status, ->(s) {
     rel = joins(:status)
 

app/views/layouts/application.html.erb
@@ -50,7 +50,7 @@ end %>
 		   <ul>
 	         <li><%= link_to "Libraries (home)", libraries_path %></li>
 		     <% if user_signed_in? && !current_user.try(:admin?) %>
-	           <li><%= link_to "My Reservation", reservations_path %></li>
+	           <li><%= link_to "My Reservation", reservations_user_path(current_user) %></li>
 	         <% end %>
 
 			 <% if current_user.try(:admin?) %>

app/views/reports/index.html.erb
@@ -1,57 +1,1 @@
 <% content_for :header do %>Reporting and Stats<% end %>
-<div class="reports collection-list">
-  <!-- Pending Reservations -->
-  <h2 class="collapsible pending_reservations">Pending Reservations - <%= @assets.collect{|a| a.reservations_pending}.flatten.length %></h2>
-  <div class="pending_reservations pending_reservations-target">
-	<% unless @libraries.blank? %>
-	  <% @libraries.each do |library| %>
-	    <h2><%= library.name %></h2>
-	      <% unless library.reservable_asset_types.blank? %>
-	        <% library.reservable_asset_types.each do |rat| %>
-			<h3 class="collapsible asset_type"><%= rat.name %> - <%= rat.reservable_assets.collect{|a| a.reservations_pending}.flatten.length %></h3>
-			<ul class="asset_type asset_type-target">
-			  <% unless rat.reservable_assets.blank? %>
-			    <% rat.reservable_assets.each do |ra| %>
-				  <% unless ra.reservations_pending.blank? %>
-				    <h3>Asset: <%= link_to(ra.name, ra) %></h3>
-				    <%= render :partial => 'shared/lines/reservation', :collection => ra.reservations_pending %>
-				  <% end %>
-			    <% end %>
-			  <% else %>
-			    <li class="empty">No pending reservations.</li>
-			  <% end %>
-			</ul>
-          <% end %>
-        <% end %>
-      <% end %>
-    <% end %>
-  </div>
-
-	<!-- Expired Reservations -->
-	<h2 class="collapsible expired_reservations">Recently Expired Reservations - <%= @assets.collect{|a| a.reservations_recently_expired}.flatten.length %></h2>
-	<div class="expired_reservations expired_reservations-target">
-	  	<% unless @libraries.blank? %>
-		  <% @libraries.each do |library| %>
-		    <h2><%= library.name %></h2>
-		      <% unless library.reservable_asset_types.blank? %>
-		        <% library.reservable_asset_types.each do |rat| %>
-				<h3 class="collapsible asset_type_3"><%= rat.name %> - <%= rat.reservable_assets.collect{|a| a.reservations_recently_expired}.flatten.length %></h3>
-				<ul class="asset_type_3 asset_type_3-target">
-				  <% unless rat.reservable_assets.blank? %>
-				    <% rat.reservable_assets.each do |ra| %>
-					  <% unless ra.reservations_recently_expired.blank? %>
-					    <h3><%= ra.name %></h3>
-					    <%= render :partial => 'shared/lines/reservation', :collection => ra.reservations_recently_expired %>
-					  <% end %>
-				    <% end %>
-				  <% else %>
-				    <li class="empty">No recently expired reservations.</li>
-				  <% end %>
-				</ul>
-	          <% end %>
-	        <% end %>
-	      <% end %>
-	    <% end %>
-	</div>
-
-</div>

app/views/reservations/destroy.js.erb
@@ -1,5 +1,7 @@
 (function () {
   var sel = '#reservation-' + <%= @reservation.id %>;
+  var num_to_dec = $(sel).closest('ul').prev().find('.count');
+  num_to_dec.text(num_to_dec.text() - 1);
   if ($(sel).siblings('li').length == 0) {
     $(sel).replaceWith('<li class="empty">No reservations.</li>');
   }

app/views/reservations/index.html.erb
@@ -1,30 +1,24 @@
 <% content_for :header do %>Reservations<% end %>
 <div class="reservations collection-list">
-
-  <% if current_user.admin? %>
-  <h2 class="action-button"><%= link_to('New Reservation', new_reservation_path) %></h2>
-  <% end %>
-
-
   <% @libraries.each do |l| %>
     <div>
       <h2><%= l.name %></h2>
       <div class="lib" id="library-<%= l.id %>">
-      <% l.reservable_asset_types.each do |rat| %>
-        <h3><%= rat.name %></h3>
-        <div class="rat" id="rat-<%= rat.id %>">
-        <% res = @reservations[l.id][rat.id] %>
-        <% ['Pending', 'Expired', 'Approved'].each do |status| %>
-          <% unless res[status].blank? %>
-          <h2 class='collapsible'><%= status %> Reservations <%= raw "(<span class='count'>#{res[status].length}</span>)" %></h2>
-          <ul class="<%= status.downcase %>">
-	          <%= render :partial => 'shared/lines/reservation', :collection => res[status] %>
-          </ul> <!-- end <%= status.downcase %> -->
-          <% end %>
-        <% end %>
-      </div> <!-- END rat-<%= rat.id %> -->
-      <% end %>
-      <!-- END library-<%= l.id %> -->
-  <% end %>
-
+        <% l.reservable_asset_types.each do |rat| %>
+          <h3><%= rat.name %></h3>
+          <div class="rat" id="rat-<%= rat.id %>">
+            <% res = @reservations[l.id][rat.id] %>
+            <% ['Pending', 'Expired', 'Approved'].each do |status| %>
+              <% unless res[status].blank? %>
+                <h2 class='collapsible'><%= status %> Reservations <%= raw "(<span class='count'>#{res[status].length}</span>)" %></h2>
+                <ul class="<%= status.downcase %>">
+	                <%= render :partial => 'shared/lines/reservation', :collection => res[status] %>
+                </ul> <!-- end <%= status.downcase %> -->
+              <% end %>  <!-- END unless -->
+            <% end %> <!-- END [status,...].each -->
+          </div> <!-- END .rat -->
+        <% end %> <!-- END l.reservable_asset_types.each -->
+      </div> <!-- END .lib -->
+    </div>
+  <% end %> <!-- END @libraries.each -->
 </div>

app/views/shared/lines/_reservation.html.erb
@@ -1,10 +1,10 @@
 <li class="reservation line" id="reservation-<%= reservation.id %>">
-  ID: <%= link_to("#{reservation.id}", (reservation.allow_edit?(current_user) || current_user.admin? ? edit_reservation_path(reservation) : reservation) ) %>,
-  User: <%= link_to("#{[reservation.user.first_name, reservation.user.last_name].join(' ')} (#{reservation.user.email})", reservation.user) %>,
+  ID: <%= link_to("#{reservation.id}", (reservation.allow_edit?(current_user) || current_user.try(:admin?) ? edit_reservation_path(reservation) : reservation) ) %>,
+  User: <%= link_to("#{[reservation.user.first_name.to_s, reservation.user.last_name.to_s].join(' ')} (#{reservation.user.email.to_s})", reservation.user) %>,
   <%= "from #{reservation.start_date} to #{reservation.end_date}" %>
   <ul class="actions">
-    <% if current_user.admin? || reservation.allow_edit?(current_user) %>
-      <% if reservation.status_id == @statuses['Pending'] %>
+    <% if current_user.try(:admin?) || reservation.allow_edit?(current_user) %>
+      <% if reservation.status_id == @statuses['Pending'] && current_user.try(:admin?) %>
         <li><%= link_to('Approve', approve_reservation_path(reservation), :class => "action-approve", :status_id => @statuses['Approved'], :approve => true, :remote => true, :method => :put) %></li>
       <% end %>
 

app/views/users/reservations.html.erb
@@ -1,0 +1,11 @@
+<% content_for :header do %>My Reservations<% end %>
+<div class="reservations collection-list">
+  <% ['Pending', 'Approved'].each do |status| %>
+    <% unless @reservations[status].blank? %>
+      <h2><%= status %> Reservations</h2>
+      <ul class="<%= status.downcase %>">
+	      <%= render :partial => 'shared/lines/reservation', :collection => @reservations[status] %>
+      </ul>
+    <% end %> <!-- END unless -->
+  <% end %> <!-- END each -->
+</div>

config/routes.rb
@@ -26,6 +26,9 @@ Inscriptio::Application.routes.draw do
   devise_for :users
 
   resources :users do
+    member do
+      get 'reservations'
+    end
     collection do
       post 'import'
       get 'export'