69814de661e81c7698c6088e48f136a60783cbc6
Dave Mayo <dave_mayo@harvard.edu>
2014-01-21 17:12:14 -0500

    LTSINSCRIP-52 First report working; number of active assets, with ability to define bounding horizons.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,12 +1,26 @@
 class ReportsController < ApplicationController
+  require 'CSV'
   before_filter :authenticate_admin!
-  
+
   def index
-    @libraries = Library.all
-    @assets = ReservableAsset.all
-    @reservable_asset_types = ReservableAssetType.all
-    
+    @actions = self.action_methods.reject {|am| am == 'index'}
     breadcrumbs.add 'Reports'
   end
-  
+
+  def active_carrels
+    start_horizon = !params[:active_carrels][:start_horizon].blank? ? Date.strptime(params[:active_carrels][:start_horizon], "%m/%d/%Y") : nil
+    end_horizon = !params[:active_carrels][:end_horizon].blank? ? Date.strptime(params[:active_carrels][:end_horizon], "%m/%d/%Y") : nil
+
+    result = Report.active_carrels(start_horizon, end_horizon)
+    respond_to do |format|
+      format.csv do
+        csv = CSV.generate do |csv|
+          result.each do |el|
+            csv << el
+          end
+        end
+        render :text => csv
+      end
+    end
+  end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -2,4 +2,40 @@ class Report
   include ActiveModel::Validations
   include ActiveModel::Conversion
   extend ActiveModel::Naming
+
+  def self.active_carrels (start_horizon = nil, end_horizon = nil)
+    # Only bound search if args exist and are datey
+    start_end_clause = ''
+    if start_horizon.is_a? Date
+      start_end_clause += "AND end_date > '#{start_horizon.to_s}'"
+    end
+    if end_horizon.is_a? Date
+      start_end_clause += "AND start_date < '#{end_horizon.to_s}'"
+    end
+
+    results = ActiveRecord::Base.connection.query(<<-SQL)
+     SELECT l.name AS l_name,
+       rat.name AS rat_name,
+       COUNT(ra.id) AS num_ras
+     FROM libraries l
+     JOIN  reservable_asset_types rat
+       ON l.id = rat.library_id
+     JOIN reservable_assets ra
+       ON rat.id = ra.reservable_asset_type_id
+     JOIN (SELECT reservable_assets.id,
+                  count(reservations.id) AS num_res
+           FROM reservable_assets, reservations, statuses
+           WHERE reservable_assets.id = reservations.reservable_asset_id
+             AND reservations.deleted_at IS NULL
+             AND reservations.status_id = statuses.id
+             AND statuses.name IN ('Pending', 'Approved')
+             #{start_end_clause}
+           GROUP BY reservable_assets.id) r
+       ON ra.id = r.id WHERE num_res > 0
+     GROUP BY l_name, rat_name
+    SQL
+
+    results.unshift %w(library_name, rat_name, active_carrels_during_period)
+  end
+
 end
