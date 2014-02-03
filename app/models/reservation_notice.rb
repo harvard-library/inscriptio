2204826ccf2b78846484b725272ea9d9ef86ef71
Dave Mayo <dave_mayo@harvard.edu>
2014-02-03 15:19:36 -0500

    LTSINSCRIP-103 - Default content for notices, default reply-to supplied by library from field

--- app/models/reservation_notice.rb
+++ app/models/reservation_notice.rb
@@ -8,11 +8,26 @@ class ReservationNotice < ActiveRecord::Base
 
   validates_presence_of :subject, :message
 
+  DEFAULT_MESSAGES = {
+    "Approved" => "Your reservation has been approved. If you have any questions please reply to this email.",
+    "Pending" => "Thank you for your application. It is currently pending and you will be notified when it has been approved. If you have any questions please reply to this email.",
+    "Declined" => "Your application has been declined. Please contact the office to which you submitted this application for further information or reply to this email.",
+    "Waitlist" => "Your application has been wait-listed. Please contact the office to which you submitted this application with any questions or reply to this email.",
+    "Expired" => "Your reservation has expired. If you have any questions please reply to this email.",
+    "Expiring" => "Your reservation is expiring soon. Please log in to your account if you would like to renew. If you have any questions please reply to this email.",
+    "Cancelled" => "Your reservation has been cancelled. If you have any questions please reply to this email.",
+    "Renewal confirmation" => "Your reservation has been renewed. If you have any questions please reply to this email."
+
+  }
+
   def self.regenerate_notices(rat)
     ReservationNotice.destroy_all(:reservable_asset_type_id => rat.id)
 
+
     Status.all.each do |s|
-      notice = ReservationNotice.new(:library => rat.library, :reservable_asset_type => rat, :status => s, :subject => s.name, :message => s.name)
+      message = DEFAULT_MESSAGES[s.name] ? DEFAULT_MESSAGES[s.name] : s.name
+      reply_to = rat.library.from ? rat.library.from : nil
+      notice = ReservationNotice.new(:library => rat.library, :reservable_asset_type => rat, :status => s, :subject => s.name, :message => message, :reply_to => reply_to)
       notice.save
       puts "Successfully created #{notice.subject}"
     end
