d44629104e7a7196fb8c661a5702a0bffb1b3010
Dave Mayo <dave_mayo@harvard.edu>
2014-06-24 10:44:32 -0400

    LTSINSCRIP-186 - Make "can I reserve it" coloring take into account user types

--- app/controllers/floors_controller.rb
+++ app/controllers/floors_controller.rb
@@ -78,7 +78,7 @@ class FloorsController < ApplicationController
             :y1 => asset.y1,
             :x2 => asset.x2,
             :y2 => asset.y2,
-            :allow_reservation => !asset.full?
+            :allow_reservation => !asset.full? && can?(:reserve, asset)
           }
         }
         render :json => output
