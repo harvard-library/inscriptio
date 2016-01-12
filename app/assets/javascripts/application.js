4e369215ab69c8918bdc8467981be236e5580a80
bobbi-SMR <bobbi_fox@harvard.edu>
2016-01-12 15:16:21 -0500

    Use new upgrade jquery-ui syntax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,11 @@
 /*
 *= require jquery
 *= require jquery_ujs
-*= require jquery.ui.draggable
-*= require jquery.ui.resizable
-*= require jquery.ui.datepicker
-*= require jquery.ui.effect.all
-*= require jquery.ui.autocomplete
+*= require jquery-ui/draggable
+*= require jquery-ui/resizable
+*= require jquery-ui/datepicker
+*= require jquery-ui/effect.all
+*= require jquery-ui/autocomplete
 *= require_self
 *= require_tree .
 */
